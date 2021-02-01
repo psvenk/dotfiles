5448e407b257fa83385527380eac95a82ea30d47
psvenk <45520974+psvenk@users.noreply.github.com>
2021-02-01 09:27:21 -0500

    Nitter userscript: change instance used
    The main Nitter instance is becoming crowded (and therefore often
    unavailable), so switch to another instance.

--- .config/qutebrowser/greasemonkey/nitter.user.js
+++ .config/qutebrowser/greasemonkey/nitter.user.js
@@ -1,17 +1,18 @@
 // ==UserScript==
 // @name Twitter to Nitter redirect
-// @description Redirect twitter.com to nitter.net
+// @description Redirect twitter.com to nitter.eu
 // @namespace psvenk
-// @version 0.1
+// @version 0.2
 // @match *://*.twitter.com/*
 // @author psvenk
 // @run-at document-start
 // @qute-js-world user
+// @grant none
 // ==/UserScript==
 
 const currentURL = window.location.toString();
 const matches = currentURL.match(/(?:\w+\.)?twitter\.com/);
 if (matches) {
-	const newURL = currentURL.replace(matches[0], "nitter.net");
+	const newURL = currentURL.replace(matches[0], "nitter.eu");
 	window.location.replace(newURL);
 }
