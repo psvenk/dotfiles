8f198a6c93a45a6561ff5dc01ae5275ca1835819
Pratyush Venkatakrishnan <contact@psvenk.com>
2023-12-28 14:34:32 -0500

    Reinstate Nitter user script
    Using the nitter.net instance again, which is back up.

---
+++ .config/qutebrowser/greasemonkey/nitter.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name Twitter to Nitter redirect
+// @description Redirect twitter.com to a Nitter instance
+// @namespace psvenk
+// @version 0.6
+// @match *://*.twitter.com/*
+// @author psvenk
+// @run-at document-start
+// @qute-js-world user
+// @grant none
+// ==/UserScript==
+
+const currentURL = window.location.toString();
+const matches = currentURL.match(/(?:\w+\.)?twitter\.com/);
+if (matches) {
+	const newURL = currentURL.replace(matches[0], "nitter.net");
+	window.location.replace(newURL);
+}
